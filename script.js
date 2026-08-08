bc9e61fa10d2074a7eb122d7522390e7a629e405
vedashri <vedashris21@gmail.com>
2026-08-08 23:50:31 +0530

    dragg and drop functionality activated

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const todo = document.querySelector('#todo');
 const  progress = document.querySelector('#progress');
 const done = document.querySelector('#done');
-
+let dragElement = null
 console.log(todo, progress, done);
 
 const tasks = document.querySelectorAll('.task');
@@ -10,6 +10,7 @@ const tasks = document.querySelectorAll('.task');
 tasks.forEach(tasks => {
   tasks.addEventListener("drag", (e)=>{
     // console.log("dragging", e);
+    dragElement = tasks;
     
   })
 })
@@ -22,6 +23,21 @@ function addDragEventsOnColumn(column) {
   column.addEventListener("dragleave", (e) => {
    e.preventDefault();
    column.classList.remove("hover-over"); 
+  })
+
+  column.addEventListener("dragover", (e) => {
+    e.preventDefault();
+    
+  })
+
+  
+  column.addEventListener("drop", (e) => {
+    e.preventDefault();
+
+    console.log("dropped", dragElement, column);
+    
+    column.appendChild(dragElement);
+    column.classList.remove("hover-over");
   })
 }
 
